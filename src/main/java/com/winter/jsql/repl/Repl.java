52124370c798a5939a31648cf01be28f588794ee
Winter <429459043@qq.com>
2020-05-17 16:07:34 +0800

    init

---
+++ src/main/java/com/winter/jsql/repl/Repl.java
@@ -0,0 +1,19 @@
+package com.winter.jsql.repl;
+
+/**
+ * 交互式解释器
+ * @author Winter
+ * @Project jsql
+ * @Package com.winter.jsql.repl
+ * @Title Repl.java
+ * @Email yanwt@vastdata.com.cn
+ * @modified
+ * @date 2020年05月17 16:02:55
+ * @Description 此处添加该类的详细说明
+ */
+public class Repl {
+
+    public static void main(String[] args) {
+
+    }
+}
